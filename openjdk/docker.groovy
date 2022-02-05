226ba2bb719e21a5d4e85db8d0e9f473fc51d235
fastjrun <fastjrun@139.com>
2022-02-05 09:50:34 +0800

    增加jdk；提升小版本；增加pipeline

---
+++ openjdk/docker.groovy
@@ -0,0 +1,47 @@
+node {
+    stage('git chekout') {
+        git branch: "master", url: 'https://gitee.com/fastjrun/pi4images.git'
+    }
+    stage('dockerFile') {
+        dir('openjdk'){
+            stash 'openjdk'
+        }
+    }
+    stage('parallel docker build') {
+        parallel (
+                'docker build && push openjdk': {
+                    sh 'cd openjdk && docker build . -t pi4k8s/openjdk8-arm64:centos7'
+                    sh 'docker push pi4k8s/openjdk8-arm64:centos7'
+                },
+                'docker build && push openjdk amd64': {
+                    node('amd64') {
+                        dir('openjdk'){
+                            unstash 'openjdk'
+                        }
+                        unstash 'openjdk'
+                        sh 'cd openjdk && docker build . -t pi4k8s/openjdk8-amd64:centos7'
+                        sh 'docker push pi4k8s/openjdk8-amd64:centos7'
+                    }
+                }
+        )
+    }
+    stage('manifest'){
+        sh 'set +e && docker manifest rm pi4k8s/openjdk8:centos7 && set -e'
+        sh 'docker manifest create pi4k8s/openjdk8:centos7 pi4k8s/tomcat9-amd64:openjdk8 pi4k8s/tomcat9-arm64:openjdk8'
+        sh 'docker manifest annotate pi4k8s/openjdk8:centos7 pi4k8s/tomcat9-amd64:openjdk8 --os linux --arch amd64'
+        sh 'docker manifest annotate pi4k8s/openjdk8:centos7 pi4k8s/tomcat9-arm64:openjdk8 --os linux --arch arm64'
+        sh 'docker manifest push pi4k8s/openjdk8:centos7'
+    }
+    stage('cleanWs'){
+        parallel (
+                'arm64': {
+                    cleanWs()
+                },
+                'amd64': {
+                    node('amd64') {
+                        cleanWs()
+                    }
+                }
+        )
+    }
+}
