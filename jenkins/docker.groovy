1e74cadb935196a7abe38bcec567b3725fc35bee
fastjrun <fastjrun@139.com>
2022-02-05 10:46:55 +0800

    增加jenkins的pipeline

---
+++ jenkins/docker.groovy
@@ -0,0 +1,51 @@
+node {
+    stage('git chekout') {
+        git branch: "master", url: 'https://gitee.com/fastjrun/pi4images.git'
+    }
+    stage('dockerFile') {
+        dir('jenkins'){
+            stash 'jenkins'
+        }
+    }
+    stage('parallel docker build') {
+        parallel (
+                'docker build && push jenkins': {
+                    sh 'cd jenkins && docker build . -t pi4k8s/jenkins-arm64:openjdk8-2.327 --build-arg CPU_ARCH=arm64'
+                    sh 'docker push pi4k8s/jenkins-arm64:openjdk8-2.327'
+                },
+                'docker build && push openjdk amd64': {
+                    node('amd64') {
+                        dir('jenkins'){
+                            unstash 'jenkins'
+                        }
+                        unstash 'jenkins'
+                        sh 'cd jenkins && docker build . -t pi4k8s/jenkins-amd64:openjdk8-2.327'
+                        sh 'docker push pi4k8s/jenkins-amd64:openjdk8-2.327'
+                    }
+                }
+        )
+    }
+    stage('manifest'){
+        try {
+            sh 'docker manifest rm pi4k8s/jenkins:openjdk8-2.327'
+        }catch(exc){
+            echo "some thing wrong"
+        }
+        sh 'docker manifest create pi4k8s/jenkins:openjdk8-2.327 pi4k8s/jenkins-amd64:openjdk8-2.327 pi4k8s/jenkins-arm64:openjdk8-2.327'
+        sh 'docker manifest annotate pi4k8s/jenkins:openjdk8-2.327 pi4k8s/jenkins-amd64:openjdk8-2.327 --os linux --arch amd64'
+        sh 'docker manifest annotate pi4k8s/jenkins:openjdk8-2.327 pi4k8s/jenkins-arm64:openjdk8-2.327 --os linux --arch arm64'
+        sh 'docker manifest push pi4k8s/jenkins:openjdk8-2.327'
+    }
+    stage('cleanWs'){
+        parallel (
+                'arm64': {
+                    cleanWs()
+                },
+                'amd64': {
+                    node('amd64') {
+                        cleanWs()
+                    }
+                }
+        )
+    }
+}
