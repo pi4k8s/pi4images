901e73c8f99ba5085c386421b51077cbe596f318
fastjrun <fastjrun@139.com>
2022-04-28 14:50:32 +0800

    新增ansible-2.9.27镜像

---
+++ ansible/README.md
@@ -0,0 +1,4 @@
+# ansible
+
+- 基于pi4k8s/centos:7构建
+- ansible-2.9.27
---
+++ ansible/docker.groovy
@@ -0,0 +1,51 @@
+node {
+    stage('git chekout') {
+        git branch: "master", url: 'https://gitee.com/fastjrun/pi4images.git'
+    }
+    stage('dockerFile') {
+        dir('ansible'){
+            stash 'ansible'
+        }
+    }
+    stage('parallel docker build') {
+        parallel (
+                'docker build && push ansible': {
+                    sh 'cd openjdk && docker build . -t pi4k8s/ansible-arm64:centos7'
+                    sh 'docker push pi4k8s/ansible-arm64:centos7'
+                },
+                'docker build && push ansible amd64': {
+                    node('amd64') {
+                        dir('ansible'){
+                            unstash 'ansible'
+                        }
+                        unstash 'ansible'
+                        sh 'cd ansible && docker build . -t pi4k8s/ansible-amd64:centos7'
+                        sh 'docker push pi4k8s/ansible-amd64:centos7'
+                    }
+                }
+        )
+    }
+    stage('manifest'){
+        try {
+            sh 'docker manifest rm pi4k8s/ansible:centos7'
+        }catch(exc){
+            echo "some thing wrong"
+        }
+        sh 'docker manifest create pi4k8s/ansible:centos7 pi4k8s/ansible-amd64:centos7 pi4k8s/ansible-arm64:centos7'
+        sh 'docker manifest annotate pi4k8s/ansible:centos7 pi4k8s/ansible-amd64:centos7 --os linux --arch amd64'
+        sh 'docker manifest annotate pi4k8s/ansible:centos7 pi4k8s/ansible-arm64:centos7 --os linux --arch arm64'
+        sh 'docker manifest push pi4k8s/ansible:centos7'
+    }
+    stage('cleanWs'){
+        parallel (
+                'arm64': {
+                    cleanWs()
+                },
+                'amd64': {
+                    node('amd64') {
+                        cleanWs()
+                    }
+                }
+        )
+    }
+}
