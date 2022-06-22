76eb72519ff836a64659ef903f02b9c7cbcb6cfd
fastjrun <fastjrun@139.com>
2022-06-22 17:53:13 +0800

    重构了ansible镜像，支持部署helm

--- ansible/README.md
+++ ansible/README.md
@@ -1,4 +1,14 @@
 # ansible
 
 - 基于pi4k8s/centos:7构建
-- ansible-2.9.27
+- git
+- python3-pip
+- pip 分别安装 ansible-2.10.7，3.4.0和4.10.0
+- ansible-galaxy 安装 community.kubernetes
+
+### name:tag
+- pi4k8s/ansible-centos7-amd64:2.10.7 == pi4k8s/ansible:centos7 
+  - 已知缺陷：x86架构服务器docker部署的jenkins的pipeline不支持部署helm应用
+- pi4k8s/ansible-centos7-amd64:3.4.0 
+- pi4k8s/ansible-centos7-amd64:4.10.0 
+
--- ansible/docker.groovy
+++ ansible/docker.groovy
@@ -1,40 +1,49 @@
 node {
+    parameters {
+        choice(
+                description: '你需要选择哪个版本进行构建 ?',
+                name: 'ansible_version',
+                choices: ['2.10.7', '3.4.0', '4.10.0']
+        )
+    }
     stage('git chekout') {
-        git branch: "master", url: 'https://gitee.com/fastjrun/pi4images.git'
+        git credentialsId: 'gogs11001', branch: "master", url: 'http://gogs.fastjrun.com:11001/devops/myDocker.git'
     }
     stage('dockerFile') {
-        dir('ansible'){
+        dir('common/ansible'){
             stash 'ansible'
         }
     }
     stage('parallel docker build') {
         parallel (
                 'docker build && push ansible': {
-                    sh 'cd ansible && docker build . -t pi4k8s/ansible-arm64:centos7'
-                    sh 'docker push pi4k8s/ansible-arm64:centos7'
+                    echo "${params.ansible_version}"
+                    sh "cd common/ansible && docker build . -t pi4k8s/ansible-centos7-arm64:${params.ansible_version} --build-arg ANSIBLE_VERSION=${params.ansible_version}"
+                    sh "docker push pi4k8s/ansible-centos7-arm64:${params.ansible_version}"
                 },
                 'docker build && push ansible amd64': {
                     node('amd64') {
-                        dir('ansible'){
+                        dir('common/ansible'){
                             unstash 'ansible'
                         }
                         unstash 'ansible'
-                        sh 'cd ansible && docker build . -t pi4k8s/ansible-amd64:centos7'
-                        sh 'docker push pi4k8s/ansible-amd64:centos7'
+                        echo "${params.ansible_version}"
+                        sh "cd common/ansible && docker build . -t pi4k8s/ansible-centos7-amd64:${params.ansible_version} --build-arg ANSIBLE_VERSION=${params.ansible_version}"
+                        sh "docker push pi4k8s/ansible-centos7-amd64:${params.ansible_version}"
                     }
                 }
         )
     }
     stage('manifest'){
         try {
-            sh 'docker manifest rm pi4k8s/ansible:centos7'
+            sh "docker manifest rm pi4k8s/ansible-centos7:${params.ansible_version}"
         }catch(exc){
             echo "some thing wrong"
         }
-        sh 'docker manifest create pi4k8s/ansible:centos7 pi4k8s/ansible-amd64:centos7 pi4k8s/ansible-arm64:centos7'
-        sh 'docker manifest annotate pi4k8s/ansible:centos7 pi4k8s/ansible-amd64:centos7 --os linux --arch amd64'
-        sh 'docker manifest annotate pi4k8s/ansible:centos7 pi4k8s/ansible-arm64:centos7 --os linux --arch arm64'
-        sh 'docker manifest push pi4k8s/ansible:centos7'
+        sh "docker manifest create pi4k8s/ansible-centos7:${params.ansible_version} pi4k8s/ansible-centos7-amd64:${params.ansible_version} pi4k8s/ansible-centos7-arm64:${params.ansible_version}"
+        sh "docker manifest annotate pi4k8s/ansible-centos7:${params.ansible_version} pi4k8s/ansible-centos7-amd64:${params.ansible_version} --os linux --arch amd64"
+        sh "docker manifest annotate pi4k8s/ansible-centos7:${params.ansible_version} pi4k8s/ansible-centos7-arm64:${params.ansible_version} --os linux --arch arm64"
+        sh "docker manifest push pi4k8s/ansible-centos7:${params.ansible_version}"
     }
     stage('cleanWs'){
         parallel (
